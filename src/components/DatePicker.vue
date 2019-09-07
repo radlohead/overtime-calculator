a317deb2aee612719c9e9372470cc960c0b618ca
= <radlohead@naver.com>
2019-09-07 19:17:10 +0900

    날짜 클릭시 팝업 오픈 (mock 으로 구현)

--- src/components/DatePicker.vue
+++ src/components/DatePicker.vue
@@ -1,7 +1,19 @@
 <template>
   <div class="root">
     <h1>야근 수당 계산</h1>
-    <datepicker :inline="true" @selected="handleSelectd"></datepicker>
+    <div class="datepicker_wrapper">
+      <datepicker :inline="true" @selected="handleSelectd"></datepicker>
+
+      <template v-if="isPopup">
+        <div class="popup">
+          <div class="popup_inner">
+            <div>출근 시간: 10:00</div>
+            <div>퇴근 시간: 22:00</div>
+            <button @click="isPopup = false">입력 완료</button>
+          </div>
+        </div>
+      </template>
+    </div>
   </div>
 </template>
 
@@ -10,7 +22,9 @@ import Datepicker from "vuejs-datepicker";
 
 export default {
   data() {
-    return {};
+    return {
+      isPopup: false
+    };
   },
   mounted() {
     console.clear();
@@ -21,6 +35,11 @@ export default {
       const getDate = date.getUTCDate();
       const getDay = date.getUTCDay();
       console.log("handleSelectd: ", getMonth, getDate);
+
+      this.openPopup();
+    },
+    openPopup() {
+      this.isPopup = true;
     }
   },
   components: {
@@ -34,5 +53,28 @@ export default {
   width: auto !important;
   max-width: 600px;
   margin: 0 auto;
+}
+</style>
+<style scoped>
+.datepicker_wrapper {
+  position: relative;
+}
+.popup {
+  position: fixed;
+  top: 0;
+  width: 100%;
+  height: 100vh;
+  background: rgba(0, 0, 0, 0.8);
+}
+.popup_inner {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  max-width: 500px;
+  padding: 15px;
+  margin: 0 auto;
+  border: 1px solid #bdbdbd;
+  background-color: #fff;
 }
 </style>
