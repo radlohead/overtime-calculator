d405f6249c9f547de1fda7efac125eb5e0e87fd2
= <radlohead@naver.com>
2019-09-07 13:39:28 +0900

    코드 정리 및 날짜 클릭시 데이터 가져오기

---
+++ src/components/DatePicker.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="root">
+    <h1>야근 수당 계산</h1>
+    <datepicker :inline="true" @selected="handleSelectd"></datepicker>
+  </div>
+</template>
+
+<script>
+import Datepicker from "vuejs-datepicker";
+
+export default {
+  data() {
+    return {};
+  },
+  mounted() {
+    console.clear();
+  },
+  methods: {
+    handleSelectd(e) {
+      console.log("handleSelectd: ", e);
+    }
+  },
+  components: {
+    Datepicker
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
