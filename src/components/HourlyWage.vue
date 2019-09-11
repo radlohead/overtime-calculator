0fb392f873d20d95234198340c54c13da85a6669
= <radlohead@naver.com>
2019-09-11 19:20:22 +0900

    시급 컴포넌트에서 다음 버튼 클릭시 라우터 이동

--- src/components/HourlyWage.vue
+++ src/components/HourlyWage.vue
@@ -1,12 +1,19 @@
 <template>
   <section class="root">
     <h2>시급을 입력하세요.</h2>
-    <input type="text" />
+    <input type="number" />
     <span>원</span>
+    <button type="button" @click="handleClickNextBtn">다음</button>
   </section>
 </template>
 <script>
-export default {};
+export default {
+  methods: {
+    handleClickNextBtn() {
+      this.$router.replace("datePicker");
+    }
+  }
+};
 </script>
 <style scoped>
 </style>
