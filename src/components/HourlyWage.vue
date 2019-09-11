7f7c75b54f2d4aa94e9ce2ef875241aa95d9a143
= <radlohead@naver.com>
2019-09-11 22:02:24 +0900

    시급 value 체크

--- src/components/HourlyWage.vue
+++ src/components/HourlyWage.vue
@@ -1,15 +1,24 @@
 <template>
   <section class="root">
     <h2>시급을 입력하세요.</h2>
-    <input type="number" />
+    <input type="number" v-model="timePay" />
     <span>원</span>
     <button type="button" @click="handleClickNextBtn">다음</button>
   </section>
 </template>
 <script>
 export default {
+  data() {
+    return {
+      timePay: null
+    };
+  },
   methods: {
     handleClickNextBtn() {
+      if (this.timePay <= 0) {
+        alert("시급을 정상적으로 입력해주세요.");
+        return;
+      }
       this.$router.replace("datePicker");
     }
   }
