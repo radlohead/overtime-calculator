86c28bfff87bb7c5bace7fed0c71512d04862269
= <radlohead@naver.com>
2019-09-29 19:50:58 +0900

    bootstrap-vue 설치

--- src/main.js
+++ src/main.js
@@ -5,6 +5,13 @@ import App from './App'
 import router from './router'
 import store from './store'
 
+import BootstrapVue from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
