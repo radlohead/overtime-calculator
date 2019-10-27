8eacf5af19998538389550c5020ada90437c212f
= <radlohead@naver.com>
2019-10-27 15:44:37 +0900

    시급 설정페이지 시급 설정 버그 및 예외처리 수정

--- src/components/HourlyWage.vue
+++ src/components/HourlyWage.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="root">
     <h1 class="title">시급 설정</h1>
-    <b-form-input type="number" class="timePayInput" v-model="tempTimePay" placeholder="시급을 입력하세요."></b-form-input>
+    <b-form-input class="timePayInput" v-model="tempTimePay" placeholder="시급을 입력하세요."></b-form-input>
     <div class="btn-box">
       <b-button variant="success" @click="handleClickNextBtn">확인</b-button>
     </div>
@@ -23,12 +23,25 @@ export default {
     if (!timePay) return;
     this.$router.replace("datePicker");
   },
+  watch: {
+    tempTimePay: function(e) {
+      this.tempTimePay = this.numberWithCommas(e);
+    }
+  },
   methods: {
+    numberWithCommas(x) {
+      return x
+        .toString()
+        .replace(/,/g, "")
+        .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    },
     handleClickNextBtn() {
-      if (this.tempTimePay <= 0) {
+      if (!this.tempTimePay) {
         alert("시급을 정상적으로 입력해주세요.");
         return;
       }
+      this.tempTimePay = this.tempTimePay.replace(/\,/g, "");
+
       localStorage.setItem("timePay", this.tempTimePay);
       this.$router.replace("datePicker");
     },
