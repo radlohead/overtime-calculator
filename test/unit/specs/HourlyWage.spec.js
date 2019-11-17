b4366a0c29f71c688144a21db581bbe8cd9ef1f8
= <radlohead@naver.com>
2019-11-17 13:39:42 +0900

    test 실행시 에러 이슈 수정

---
+++ test/unit/specs/HourlyWage.spec.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import HourlyWage from '@/components/HourlyWage.vue'
+
+describe('HourlyWage.vue', () => {
+  it('should render correct contents', () => {
+    const Constructor = Vue.extend(HourlyWage)
+    const vm = new Constructor().$mount()
+    expect(vm.$el.querySelector('.hello h1').textContent).toEqual(
+      'Welcome to Your Vue.js App'
+    )
+  })
+})
