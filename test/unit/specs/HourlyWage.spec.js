3749dfd9dd2bcc6cbca32a78d45b70f6467d34a9
= <radlohead@naver.com>
2019-11-19 23:44:39 +0900

    시급설정 페이지 테스트 코드

--- test/unit/specs/HourlyWage.spec.js
+++ test/unit/specs/HourlyWage.spec.js
@@ -2,11 +2,12 @@ import Vue from 'vue'
 import HourlyWage from '@/components/HourlyWage.vue'
 
 describe('HourlyWage.vue', () => {
-  it('should render correct contents', () => {
+  it('Function numberWithCommas', () => {
     const Constructor = Vue.extend(HourlyWage)
     const vm = new Constructor().$mount()
-    expect(vm.$el.querySelector('.hello h1').textContent).toEqual(
-      'Welcome to Your Vue.js App'
-    )
+
+    expect(vm.numberWithCommas(1000)).toBe('1,000')
+    expect(vm.numberWithCommas(10000)).toBe('10,000')
+    expect(vm.numberWithCommas(1000000)).toBe('1,000,000')
   })
 })
